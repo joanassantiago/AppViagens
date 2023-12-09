d4bd0e3f8af931d5a109f688ded7f0fccad686b2
joana <joanassantiago@ua.pt>
2023-12-09 15:55:52 +0000

    2

--- appviagens.py
+++ appviagens.py
@@ -4,18 +4,37 @@ import requests
 
 
 #serve para verifcar se a atração que a pessoa deseja esta contida na lista de categorias que foi dada pelos professores
-def obter_atrações():
-    atrações_existentes = []
-    with open("categories.txt", "r") as atrações:
-        for linhas in atrações.readlines():
-            atrações_existentes.append(linhas.strip())
+# def obter_atrações():
+#     atrações_existentes = []
+#     with open("categories.txt", "r") as atrações:
+#         for linhas in atrações.readlines():
+            
 
-with open("API_key.txt") as file:
-    api_key = file.read()
-api_key = api_key.strip()
 
-def main():
+category = input()
+lat = input()
+lon = input()
+radius = input()
 
-    localização = float(input("Localização (latitude, longitude): "))
-    raio = float(input("Distância que pode viajar (km): "))
-    atração = str(input("Tipo de atração que deseja: "))
+url = "https://api.geoapify.com/v2/places?"
+
+url += "categories=" + category + "&filter=circle:" + str(lon) + "," + str(lat) + "," + str(radius) + "&bias=proximity:" + str(lon) + "," + str(lat) 
+
+url += "&apiKey=" + "34e316823d0044c4b9725dcd1af10809"
+
+response = requests.get(url)
+print(response.status_code)
+
+#trocar variavel
+a = response.json()
+print (a)
+print (a["type"])
+# with open("API_key.txt") as file:
+#     api_key = file.read()
+# api_key = api_key.strip()
+
+# def main():
+
+#     localização = float(input("Localização (latitude, longitude): "))
+#     raio = float(input("Distância que pode viajar (km): "))
+#     atração = str(input("Tipo de atração que deseja: "))
